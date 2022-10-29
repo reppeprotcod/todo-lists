f1e7c42a53244ff2d9ce49473f8f5399b12f8b7d
Marta Turevich <martaschola@gmail.com>
2022-10-30 02:34:05 +0300

    fix build path

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use('/auth', router);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+    app.use('/', express.static(path.join(__dirname, '..', 'client', 'build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     });
 }
 
