35a23e0ab791c3c51ca4479ab28b50bce47ea0cd
Marta Turevich <martaschola@gmail.com>
2022-10-31 02:23:08 +0300

    change base url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export const conf = {
-    // base_url: "https://td-lists.herokuapp.com"
-    base_url: "http://localhost:4000"
+    base_url: "https://td-lists.herokuapp.com"
+    //base_url: "http://localhost:4000"
 }
